48826064b2384cd14034318dd9b9a204fb10b8c3
18z <xspiritualx@gmail.com>
2016-05-14 19:12:40 +0800

    simple module for sixense

--- parser-test/plugins.py
+++ parser-test/plugins.py
@@ -1,9 +1,12 @@
 def load_modules():
 
     from print_url import print_url
+    plugins = dict()
 
     urltp = print_url()
 
-    return urltp
+    plugins['print_url'] = urltp
+
+    return plugins
 
 __modules__ = load_modules()
--- parser-test/watcher.py
+++ parser-test/watcher.py
@@ -6,4 +6,4 @@ clipboard_content = "http://www.google.com.tw"
 # print __parser__.url(clipboard_content)
 
 if __parser__.url(clipboard_content) is True:
-    print __modules__.run(clipboard_content)
+    __modules__['print_url'].run(clipboard_content)
