9b15f712530f05d837d147291e4ec700f3dfaaee
18z <xspiritualx@gmail.com>
2016-05-16 10:38:57 +0800

    add watcher_no_thread.py

---
+++ parser-test/watcher_no_thread.py
@@ -0,0 +1,38 @@
+import time
+import threading
+import pyperclip
+
+from parser import __parser__
+from plugins import __modules__
+
+class ClipboardWatcher(object):
+    def __init__(self, pause):
+        super(ClipboardWatcher, self).__init__()
+        self._pause = pause
+        self._stopping = False
+
+    def run(self):
+        recent_value = ""
+        while not self._stopping:
+            if pyperclip.paste() != recent_value:
+                recent_value = pyperclip.paste()
+                print "print from watcher.run() "+recent_value
+                if __parser__.url(pyperclip.paste()) is True:
+                    __modules__['print_url'].run(pyperclip.paste())
+            time.sleep(self._pause)
+
+    def stop(self):
+        self._stopping = True
+
+if __name__ == "__main__":
+
+    watcher = ClipboardWatcher(1)
+    watcher.run() # same as calling watcher.run()
+
+    while True:
+        try:
+            # print "Waiting for changed clipboard..."
+            time.sleep(5)
+        except KeyboardInterrupt:
+            watcher.stop()
+            break
