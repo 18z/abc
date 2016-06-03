b06274a80cc4eb0c3936ae846af0e633a307d5ef
18z <xspiritualx@gmail.com>
2016-06-03 15:08:44 +0800

    update t.py

--- t.py
+++ t.py
@@ -2,16 +2,16 @@ class suite(object):
 
     umbrella = "black_unbrella"
 
-    def __init__(self):
-        self.tshirt = "sizeM"
-        self.shoes = "9.5"
-        self.pant = "30"
+    def __init__(self, tshirt_size, shoe_size, pant_size):
+        self.tshirt = tshirt_size
+        self.shoes = shoe_size
+        self.pant = pant_size
 
     def show_suite(self):
         print (self.tshirt, self.shoes, self.pant)
 
 
-s = suite()
+s = suite("M", "10.5", "33")
 
 s.show_suite()
 print(s.umbrella)
