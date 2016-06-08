63028c7b04ce41ca47fc0a33488fedbc6582a4ca
18z <xspiritualx@gmail.com>
2016-06-09 02:05:39 +0800

    update bss.py

--- bss.py
+++ bss.py
@@ -22,7 +22,13 @@ soup = BeautifulSoup.BeautifulSoup(urllib.urlopen(s1))
 #             print(a.get('href'))
 
 
-content = soup.findAll("div", {"class": "content"})[0]
-for tag_a in content.findAll("a"):
-    if tag_a.get('class') == "down":
-        print(tag_a.get('href'))
+# content = soup.findAll("div", {"class": "content"})[0]
+# for tag_a in content.findAll("a"):
+#     if tag_a.get('class') == "down":
+#         print(tag_a.get('href'))
+
+list1 = soup.findAll("div", {"class": "w240 mt-12 mr-15"})[0]
+print list1.find("div", {"class": "title"}).h3.string
+for a in list1.findAll("a"):
+    if a.get('class') == "down":
+        print(a.get('href'))
