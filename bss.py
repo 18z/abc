a633b12657dfda2f07f05dbfee1941daa4650e2f
18z <xspiritualx@gmail.com>
2016-06-09 14:10:22 +0800

    update bss.py

--- bss.py
+++ bss.py
@@ -27,8 +27,20 @@ soup = BeautifulSoup.BeautifulSoup(urllib.urlopen(s1))
 #     if tag_a.get('class') == "down":
 #         print(tag_a.get('href'))
 
-list1 = soup.findAll("div", {"class": "w240 mt-12 mr-15"})[0]
-print list1.find("div", {"class": "title"}).h3.string
-for a in list1.findAll("a"):
-    if a.get('class') == "down":
-        print(a.get('href'))
+# list1 = soup.findAll("div", {"class": "w240 mt-12 mr-15"})[0]
+# print list1.find("div", {"class": "title"}).h3.string
+# print list1.find("div", {"class": "tab"}).span
+# for a in list1.findAll("a"):
+#     if a.get('class') == "down":
+#         print(a.get('href'))
+
+
+w240 = soup.findAll("div", {"class": "w240 mt-12 mr-15"})[0]
+print w240.find("div", {"class": "title"}).h3.string
+tab = w240.findAll("div", {"class": "tab"})
+
+print tab[0].span.string
+toplist = w240.findAll("div", {"class": "toplist ami"})
+for url in toplist[0].findAll("a"):
+    if url.get('class') == "down":
+        print(url.get('href'))
