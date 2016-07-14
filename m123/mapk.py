ac6408088f5d99497390db5018b59e985e105781
18z <xspiritualx@gmail.com>
2016-07-14 15:23:09 +0800

    update mapk.py

--- m123/mapk.py
+++ m123/mapk.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 # from core.config import import *
 
-# from core.db.MongoMongo import DB
+from core.db.Mongo import DB
 from common.objects import File
 # from common.helpers import sizeof_fmt, print_header_line, print_result_line
 
@@ -20,52 +20,105 @@ today = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
 soup = BeautifulSoup.BeautifulSoup(urllib.urlopen(s1))
 
 
+def insert_into_db(result):
+    # try:
+    DB().insert_apk(result)
+    # bar.next()
+    # except KeyError:
+    #     logging.warn(
+    #         "Maybe the apk already exists: {}".format(result['pgname']))
+    # continue
+    #     raise
+    # except:
+    #     logging.error("DB insert error: {}".format(result['pgname']))
+    # raise
+
+
 def generate_apk_info(targetapk):
+
     result = {}
     result['vt_scan'] = False
     result['submit_date'] = today
     result['source'] = 'm.apk'
-    # result['title'] = cat
-    # result['sub_title'] = ctr
-    # result['name'] = unicode(i.title).encode('utf8')
-    # result['rank'] = rank
-    # result['pgname'] = i.details.appDetails.packageName
-    # result['version'] = i.details.appDetails.versionCode
-    # result['size'] = sizeof_fmt(i.details.appDetails.installationSize)
-    # result['upload_date'] = datetime.strptime(unicode(i.details.appDetails.uploadDate).encode('utf8'),
-    #                                           locale_timestring[LANG]).strftime('%Y-%m-%d')
+    result['title'] = category
+    result['sub_title'] = subcategory
+    result['name'] = unicode(apk_name).encode('utf8')
+    result['rank'] = ""
+    result['pgname'] = pkg_name
+    result['version'] = version
+    result['size'] = size
+    result['upload_date'] = ""
     # Download APK
     result['apkdata'] = targetapk.read()
 
     # Calculate file hashes
     result.update(File(result['apkdata']).result)
 
-    print result['md5']
+    print ' N ' + result['name']
+    print ' V ' + result['version']
+    print ' M ' + result['md5']
+    print ' S ' + result['size']
+    print ' P ' + result['pgname'] + "\n"
+
+    return result
 
 
 def _download_apk(dlink):
     targetapk = urllib.urlopen(dlink)
-    generate_apk_info(targetapk)
+    return targetapk
     # with open('target.apk', 'wb') as output:
     #     output.write(targetapk.read())
 
 
 def _get_d_url(durl):
-    # durl = "http://m.apk.tw/app/com.android.vending/"
     soup = BeautifulSoup.BeautifulSoup(urllib.urlopen(durl))
     dclass = soup.find("div", {"class": "download"})
     print " o " + dclass.find("a").get('href')
     dlink = dclass.find("a").get('href')
-    _download_apk(dlink)
 
+    return dlink
+
+
+def _get_apk_name(durl):
+    soup = BeautifulSoup.BeautifulSoup(urllib.urlopen(durl))
+    title = soup.find("div", {"class": "detailInfo mt-5"})
+    apk_name = title.find("h3").string
+
+    return apk_name
+
+
+def _get_apk_attr(durl):
+    soup = BeautifulSoup.BeautifulSoup(urllib.urlopen(durl))
+    ppt = soup.find("div", {"class": "property mt-10"})
+    ppts = ppt.findAll("li")
+    version = ppts[0].string
+    size = ppts[1].string
+
+    return version, size
+
+
+# category
 w240 = soup.findAll("div", {"class": "w240 mt-12 mr-15"})[0]
-print w240.find("div", {"class": "title"}).h3.string
-tab = w240.findAll("div", {"class": "tab"})
+category = w240.find("div", {"class": "title"}).h3.string
+print category
 
-print tab[0].span.string
+# sub-category
+tab = w240.findAll("div", {"class": "tab"})
+subcategory = tab[0].span.string
+print subcategory
+
+# download list
 toplist = w240.findAll("div", {"class": "toplist ami"})
 for url in toplist[0].findAll("a"):
     if url.get('class') == "down":
-        print " x " + (url.get('href'))
+        # print " x " + (url.get('href'))
+        # slicing package name
+        pkg_name = url.get('href')[20:][:-1]
+
         fakeurl = url.get('href')
-        _get_d_url(fakeurl)
+        dlink = _get_d_url(fakeurl)
+        targetapk = _download_apk(dlink)
+        apk_name = _get_apk_name(fakeurl)
+        version, size = _get_apk_attr(fakeurl)
+        result = generate_apk_info(targetapk)
+        insert_into_db(result)
