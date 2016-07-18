80918bdf0b22cf86b011e17ea6c0e861da0245f6
18z <xspiritualx@gmail.com>
2016-07-18 15:49:20 +0800

    add vt.py web.py

---
+++ m123/web.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+if __name__ == '__main__':
+
+    from flask import Flask
+    from core.flaskweb.ui.view import ui
+
+    portal = Flask(__name__, template_folder='core/flaskweb/templates', static_folder='core/flaskweb/static')
+    portal.config.from_object('core.config')
+
+    portal.register_blueprint(ui)
+
+    portal.run(host='127.0.0.1', port=8000, )
