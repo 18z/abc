7a0b471c796be4d67fa1376e1b26a7aee1b919e9
18z <xspiritualx@gmail.com>
2016-10-21 15:27:52 +0800

    auto check library

--- in.py
+++ in.py
@@ -4,4 +4,13 @@ import pprint
 import androguard
 
 print inspect.getsourcefile(androguard)
+if "site-packages" in inspect.getsourcefile(androguard):
+    print "third party library"
+else:
+    print "standard library"
+
 print inspect.getsourcefile(os)
+if "site-packages" in inspect.getsourcefile(os):
+    print "third party library"
+else:
+    print "standard library"
