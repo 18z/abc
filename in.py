29c75f98c9f2bdeb917e118ad79659fdb0e55133
18z <xspiritualx@gmail.com>
2016-10-21 16:24:56 +0800

    update in.py using globals

--- in.py
+++ in.py
@@ -1,16 +1,35 @@
 import inspect
+import sys
 import os
-import pprint
 import androguard
 
-print inspect.getsourcefile(androguard)
+# print inspect.getsourcefile(androguard)
 if "site-packages" in inspect.getsourcefile(androguard):
     print "third party library"
 else:
     print "standard library"
 
 print inspect.getsourcefile(os)
-if "site-packages" in inspect.getsourcefile(os):
-    print "third party library"
-else:
-    print "standard library"
+# if "site-packages" in inspect.getsourcefile(sys):
+#     print "third party library"
+# else:
+#     print "standard library"
+
+# print sys.modules.keys()
+
+import types
+
+
+def imports():
+    for name, val in globals().items():
+        print name
+        # if isinstance(val, types.ModuleType):
+        # yield val.__name__
+        # print val.__name__
+
+imports
+
+for i in range(0, len(globals().items())):
+    print globals().items()[i][1]
+
+# list(imports)
